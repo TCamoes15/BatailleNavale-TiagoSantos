5dbc4074097eadea682a31b3ae790da596a1ccae
Tiago Santos <tiago.santos@cpnv.ch>
2020-03-06 14:47:35 +0100

    Afficher fichier "Aide"

--- Code/main.c
+++ Code/main.c
@@ -1,6 +1,26 @@
 #include <stdio.h>
+#include <stdlib.h>
+#pragma execution_character_set("utf-8")
+
+void aide ()
+{
+    FILE* fichier = NULL;
+    int caractereActuel = 0;
+
+    fichier = fopen("Aide//Aide.txt", "r");
+
+    if (fichier != NULL){
+            do{
+                caractereActuel = fgetc(fichier);
+                printf("%c", caractereActuel);
+            }while (caractereActuel != EOF);
+
+            fclose(fichier);
+    }
+}
 
 int main() {
-    printf("Hello, World!\n");
+
+    aide();
     return 0;
 }
